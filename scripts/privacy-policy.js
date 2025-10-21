cfaa295e32ea12191e91eb34c3825dd3f741c5b9
cad3n123 <cadencrowson@gmail.com>
2025-10-21 16:53:07 -0500

    PP looks better on mobile. New inverted tattoo image

--- scripts/privacy-policy.js
+++ scripts/privacy-policy.js
@@ -5,8 +5,17 @@ const [$privacyPolicyButton, $privacyPolicyDiv, $privacyPolicyX] = [
 ].map((id) => document.getElementById(id));
 
 $privacyPolicyButton.addEventListener('click', () => {
-  window.scrollTo(0, 0);
-  $privacyPolicyDiv.classList.add('active');
+  document.scrollingElement.scrollTo({
+    top: 0,
+    left: 0,
+    behavior: 'instant',
+  });
+
+  // 3. Use a setTimeout to give the browser a tick to process the scroll
+  //    *before* you show the absolute-positioned div.
+  setTimeout(() => {
+    $privacyPolicyDiv.classList.add('active');
+  }, 0); // 0 or 1ms is usually all it takes
 });
 $privacyPolicyX.addEventListener('click', () => {
   $privacyPolicyDiv.classList.remove('active');
